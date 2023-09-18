ff229ab79157e77237976c5330594db22949d993
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-18 20:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let doc=document.getElementById("form1");
+	let fname=doc.getElementsByTagName("INPUT")[0].value;
+	let Sname=doc.getElementsByTagName("INPUT")[1].value;
+
+	alert(fname+" "+Sname);
+	
 
 }
